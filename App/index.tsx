aea53b5e2049d0b0cc2a1dba7d86d2fe911c6657
alfrdmalr <alfrdmalr@gmail.com>
2019-08-22 15:10:01 -0400

    feat: include steps demonstrating orientation options

--- App/index.tsx
+++ App/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
 import { Walktour, Step } from '../src/index'
+import { CardinalOrientation } from '../utils/positioning';
 
 const steps: Step[] = [
   { querySelector: '#one', title: 'Title one', description: 'Description one' },
@@ -9,7 +10,11 @@ const steps: Step[] = [
   { querySelector: '#one', title: 'Back to the first element we selected: testing long values for the title and such', description: 'description two here I am testing long values for the body as well just to see how it natively handles this type of thing' },
   { querySelector: '#three', title: 'Three', description: 'another description' },
   { querySelector: '.four', title: 'CSS Selectors', description: 'now with query selectors for even better element selection!' },
-  { querySelector: '#five', title: 'Interact through the mask cutout', description: 'click the button to see for yourself!' }
+  { querySelector: '#five', title: 'Interact through the mask cutout', description: 'click the button to see for yourself!' },
+  {querySelector: '#six', title: "Cardinal Direction Positioning", description: 'East!', orientation: [CardinalOrientation.EAST]},
+  {querySelector: '#six', title: "Cardinal Direction Positioning", description: 'South!', orientation: [CardinalOrientation.SOUTH]},
+  {querySelector: '#six', title: "Get More Specific!", description: 'North with West alignment!!', orientation: [CardinalOrientation.NORTHWEST]},
+  {querySelector: '#six', title: "Get More Specific!", description: 'West with North alignment!', orientation: [CardinalOrientation.WESTNORTH]},
 ]
 
 const styleElementOne: React.CSSProperties = {
@@ -40,9 +45,27 @@ const styleElementFour: React.CSSProperties = {
   background: 'orange',
   width: 200,
   height: 100,
-  left: 600,
-  top: 1800,
+  left: 900,
+  top: 500,
   position: 'absolute'
+}
+
+const styleElementSix: React.CSSProperties = {
+  background: 'aquamarine',
+  width: 200,
+  height: 100,
+  left: 450,
+  top: 450,
+  position: 'absolute'
+}
+
+const styleElementSeven: React.CSSProperties = {
+  background: 'linear-gradient(to right, red, white, blue)',
+  width: 200,
+  height: 100,
+  left: 169,
+  top: 1776,
+  position: 'absolute',
 }
 
 
@@ -53,6 +76,8 @@ const App = () => (
     <div id={'two'} style={styleElementTwo} />
     <div id={'three'} style={styleElementThree} />
     <div className={'four'} style={styleElementFour} />
+    <div id='six' style={styleElementSix} />
+    <div id='seven' style={styleElementSeven} />
     <button
       style={{ position: 'absolute', top: '350px', left: '650px', cursor: "pointer" }} id="five"
       onClick={() => alert('you did it!')}
