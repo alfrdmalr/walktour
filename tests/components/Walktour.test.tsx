a783fabdbd2ab7dc0e1a171ab73541218eac1fb5
alfrdmalr <alfrdmalr@gmail.com>
2019-09-30 16:01:48 -0400

    update: test that portal and mask are being rendered

--- tests/components/Walktour.test.tsx
+++ tests/components/Walktour.test.tsx
@@ -1,13 +1,38 @@
 import * as React from 'react';
-import {shallow} from 'enzyme';
-import {Walktour } from '../../src/components/Walktour';
+import { shallow, mount } from 'enzyme';
+import { Walktour } from '../../src/components/Walktour';
 
-test('walktour render', () => {
-  const tour = shallow(<div>
-    <Walktour steps={[]} />
-  </div>)
+describe('Walktour render structure', () => {
+  test('walktour render', () => {
+    const tour = shallow(<Walktour steps={[]} />)
 
-  expect(tour.length).toBe(1); //should render a single "portal" container
+    expect(tour.isEmptyRender()).toBe(true); //no step content, should render null
+    expect(tour.children().length).toBe(0);
+  })
+
+  test('portal render', () => {
+    const tour = mount(
+      <Walktour steps={[{ selector: null, description: 'hello world', title: 'Hey Earth!' }]} />
+    );
+    const portal = tour.find('#walktour-portal');
+    const mask = portal.find('Mask');
+    const tooltip = portal.find('#walktour-tooltip-container')
+
+
+    expect(portal.exists()).toBe(true);
+    expect(portal.children().length).toBe(2); //mask and tooltip only
+    expect(tooltip.exists()).toBe(true);
+    expect(mask.exists()).toBe(true);
+  })
+
+  // test('with target', () => {
+  //   const tour = mount(<div>
+  //     <div id="one" />
+  //     <Walktour steps={[{ selector: '#one', description: 'hello world', title: 'Hey Earth!' }]} />
+  //   </div>)
+
+  // const tooltip = tour.find('#walktour-tooltip-container') 
   
 
+  // })
 })
