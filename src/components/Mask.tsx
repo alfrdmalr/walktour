d84c78518dd1a349b0d1bea8e653d888e3e6ee36
alfrdmalr <alfrdmalr@gmail.com>
2019-10-09 10:01:44 -0400

    refactor: update mask to SVG

--- src/components/Mask.tsx
+++ src/components/Mask.tsx
@@ -9,55 +9,71 @@ interface MaskProps {
   tourRoot: Element;
   disableMaskInteraction?: boolean;
   disableCloseOnClick?: boolean;
+  maskId: string;
 }
 
 export function Mask(props: MaskProps): JSX.Element {
-  const { target, disableMaskInteraction, padding, tourRoot, close, disableCloseOnClick } = props;
+  const { target, disableMaskInteraction, padding, tourRoot, close, disableCloseOnClick, maskId } = props;
 
   const containerBottom: number = (tourRoot && !document.body.isSameNode(tourRoot)) ? tourRoot.scrollHeight : document.documentElement.scrollHeight;
   const containerRight: number = (tourRoot && !document.body.isSameNode(tourRoot)) ? tourRoot.scrollWidth : document.documentElement.scrollWidth;
 
-  let maskStyle: React.CSSProperties = {
-    position: 'absolute',
-    backgroundColor: 'rgba(0, 0, 0, 0.3)',
-    pointerEvents: 'auto',
-    top: 0,
-    left: 0,
+  const pathId = `clip-path-${maskId}`;
+
+
+  const getCutoutPoints = (target: HTMLElement): string => {
+    if (!target) {
+      return;
+    }
+
+    const targetData: ClientRect = target.getBoundingClientRect();
+    const coords: Coords = getTargetPosition(tourRoot, target);
+
+    const cutoutTop: number = coords.y - padding;
+    const cutoutLeft: number = coords.x - padding;
+    const cutoutRight: number = coords.x + targetData.width + padding;
+    const cutoutBottom: number = coords.y + targetData.height + padding;
+
+    return `0 0, 
+            0 ${containerBottom}, 
+            ${cutoutLeft} ${containerBottom}, 
+            ${cutoutLeft} ${cutoutTop}, 
+            ${cutoutRight} ${cutoutTop}, 
+            ${cutoutRight} ${cutoutBottom}, 
+            ${cutoutLeft} ${cutoutBottom}, 
+            ${cutoutLeft} ${containerBottom}, 
+            ${containerRight} ${containerBottom}, 
+            ${containerRight} 0`;
+  }
+
+  const svgStyle: React.CSSProperties = {
     height: containerBottom,
     width: containerRight,
+    pointerEvents: disableMaskInteraction ? 'auto' : 'none'
   }
 
-  if (!target) {
-    return <div style={maskStyle} />
-  }
+  return (
+    <svg style={svgStyle}>
+      {target &&
+        <defs>
+          <clipPath id={pathId}>
+            <polygon points={getCutoutPoints(target)}
+            />
+          </clipPath>
+        </defs>
+      }
 
-  const targetData: ClientRect = target.getBoundingClientRect();
-  const coords: Coords = getTargetPosition(tourRoot, target);
-
-  const cutoutTop: number = coords.y - padding;
-  const cutoutLeft: number = coords.x - padding;
-  const cutoutRight: number = coords.x + targetData.width + padding;
-  const cutoutBottom: number = coords.y + targetData.height + padding;
-
-  const cutoutStyle: React.CSSProperties = {
-    clipPath: `polygon(0px 0px, 0px ${containerBottom}px, ${cutoutLeft}px ${containerBottom}px, ${cutoutLeft}px ${cutoutTop}px, ${cutoutRight}px ${cutoutTop}px, ${cutoutRight}px ${cutoutBottom}px, ${cutoutLeft}px ${cutoutBottom}px, ${cutoutLeft}px ${containerBottom}px, ${containerRight}px ${containerBottom}px, ${containerRight}px 0px)`,
-  }
-
-  //cover the cutout to prevent interaction. we keep cutout to retain highlight appearance
-  const blockInteractionStyle: React.CSSProperties = {
-    position: 'absolute',
-    pointerEvents: 'auto',
-    top: cutoutTop,
-    left: cutoutLeft,
-    height: targetData.height + padding * 2,
-    width: targetData.width + padding * 2
-  }
-
-  return (<>
-    <div style={{ ...maskStyle, ...cutoutStyle }} onClick={disableCloseOnClick ? undefined : close}>
-      {disableMaskInteraction &&
-        <div style={blockInteractionStyle} />}
-    </div>
-  </>
+      <rect
+        onClick={disableCloseOnClick ? null : close}
+        x={0}
+        y={0}
+        width={containerRight}
+        height={containerBottom}
+        fill='black'
+        fillOpacity={0.3}
+        pointerEvents='auto'
+        clipPath={target ? `url(#${pathId})` : null}
+      />
+    </svg>
   );
 }
