7099cea9cdec4c2c3b6c3ead63383e56187bb5d6
alfrdmalr <alfrdmalr@gmail.com>
2019-10-21 11:52:42 -0400

    refactor: adjust position filtering for small viewports

--- src/utils/tour.ts
+++ src/utils/tour.ts
@@ -1,21 +1,15 @@
-import { Coords, dist, Dims, areaDiff } from "./dom";
+import { Coords, dist, Dims, areaDiff, fitsWithin, getElementDims } from "./dom";
 import { getTargetPosition } from "./positioning";
-import { isElementInView } from "./viewport";
+import { isElementInView, getViewportDims } from "./viewport";
 
 //miscellaneous tour utilities
 
 export function debounce<T extends any[]>(f: (...args: T) => void) {
   let functionCall: number;
-
-  console.log('debounce called')
-
   return (...args: T) => {
     if (functionCall) {
-      console.log('function canceled')
       window.cancelAnimationFrame(functionCall);
     }
-
-    console.log('function called')
     functionCall = window.requestAnimationFrame(() => f(...args));
   }
 }
@@ -27,25 +21,21 @@ export function getIdString(base: string, identifier?: string): string {
 export function shouldUpdate(tourRoot: Element, tooltip: HTMLElement, target: HTMLElement, targetPosition: Coords, targetSize: Dims, rerenderTolerance: number): boolean {
   if (!tourRoot || !tooltip) {
     return false; // bail if these aren't present; need them for calculations
+  } else if (!isElementInView(tourRoot, tooltip)) {
+    return true; //if the tooltip is off screen, always update
+  } else if (!target && !targetPosition && !targetSize) {
+    return false;  // if no target info exists, bail
+  } else if ((!target && targetPosition) || (target && !targetPosition) ||
+    (!isElementInView(tourRoot, target) && fitsWithin(getElementDims(target), getViewportDims(tourRoot)))) {
+    return true; // if the target appeared/disappeared or if the target is offscreen and can fit on the screen
+  } else {
+    const currentTargetSize: Dims = { width: target.getBoundingClientRect().width, height: target.getBoundingClientRect().height }; //TODO getelementdims
+    const currentTargetPosition: Coords = getTargetPosition(tourRoot, target);
+    const sizeChanged: boolean = areaDiff(currentTargetSize, targetSize) > rerenderTolerance;
+    const positionChanged: boolean = dist(currentTargetPosition, targetPosition) > rerenderTolerance;
+
+    return sizeChanged || positionChanged;
   }
-
-  // if the target appeared/disappeared or if the tooltip is off screen, update
-  if ((!target && targetPosition) || (target && !targetPosition) || !isElementInView(tourRoot, tooltip)) {
-    return true;
-  }
-
-  // if neither target nor target position are defined, bail
-  if (!target && !targetPosition) {
-    return false;
-  }
-
-  const currentTargetSize: Dims = { width: target.getBoundingClientRect().width, height: target.getBoundingClientRect().height }; //TODO getelementdims
-  const currentTargetPosition: Coords = getTargetPosition(tourRoot, target);
-
-  const sizeChanged: boolean = areaDiff(currentTargetSize, targetSize) > rerenderTolerance;
-  const positionChanged: boolean = dist(currentTargetPosition, targetPosition) > rerenderTolerance;
-
-  return sizeChanged || positionChanged;
 }
 
 export function clearWatcher(watcherId: React.MutableRefObject<number>): void {
