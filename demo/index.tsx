a7b1c4ec68c104dd9ee860545f16e39ec0b95af6
alfrdmalr <alfrdmalr@gmail.com>
2019-11-19 12:13:21 -0500

    chore: formatting

--- demo/index.tsx
+++ demo/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 
-import { Walktour,  WalktourLogic } from '../src/components/Walktour'
+import { Walktour, WalktourLogic } from '../src/components/Walktour'
 import { playgroundSetup, secondarySteps, primaryIntoSecondary } from './setup'
 
 const App = () => {
@@ -12,7 +12,13 @@ const App = () => {
     <>
       {playgroundSetup({ buttonText: "Toggle Tour", onButtonClick: () => setTourOpen(!tourOpen) })}
       <Walktour steps={secondarySteps()} identifier={"2"} rootSelector={"#demo-container"} disableListeners />
-      <Walktour steps={primaryIntoSecondary()} identifier={"1"} isOpen={tourOpen} customCloseFunc={(logic: WalktourLogic) => { setTourOpen(false); logic.close(); }} />
+      <Walktour
+        steps={primaryIntoSecondary()}
+        identifier={"1"}
+        isOpen={tourOpen}
+        customCloseFunc={(logic: WalktourLogic) => { setTourOpen(false); logic.close(); }}
+        disableCloseOnClick
+      />
     </>
   )
 }
