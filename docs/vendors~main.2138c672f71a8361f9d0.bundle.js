77c8e6f3f2dc7543af97a1fc4fb938c352c67d85
alfrdmalr <alfrdmalr@gmail.com>
2019-12-05 12:54:20 -0500

    update: demo build files

--- docs/vendors~main.2138c672f71a8361f9d0.bundle.js
+++ docs/vendors~main.2138c672f71a8361f9d0.bundle.js
@@ -75,4 +75,4 @@ function isObjectObject(o){return!0===
  * Released under the MIT License.
  */
 function isObject(val){return null!=val&&"object"==typeof val&&!1===Array.isArray(val)}(o)&&"[object Object]"===Object.prototype.toString.call(o)}function isPlainObject(o){var ctor,prot;return!1!==isObjectObject(o)&&("function"==typeof(ctor=o.constructor)&&(!1!==isObjectObject(prot=ctor.prototype)&&!1!==prot.hasOwnProperty("isPrototypeOf")))}__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,"default",(function(){return isPlainObject}))}]]);
-//# sourceMappingURL=vendors~main.706ab985acfc468363ff.bundle.js.map
+//# sourceMappingURL=vendors~main.2138c672f71a8361f9d0.bundle.js.map
